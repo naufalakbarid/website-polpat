8d15bc8b2b4a6236ae967cee766170dbe1c4b7e5
Naufal Akbar <naufal18akbar55@gmail.com>
2026-07-30 09:45:30 +0700

    rancangan RBAC users, ekskul, berita

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['super_admin', 'admin_ekskul'])->default('admin_ekskul');
+            $table->foreignId('ekskul_id')->nullable()->constrained('ekskuls')->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
         });
